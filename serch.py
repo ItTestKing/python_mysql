1d77b25ee95f6a856ddb3c12cd3501f5b50feb6c
Likangkang08 <1712340552@qq.com>
2019-04-10 18:46:57 +0800

    add

--- serch.py
+++ serch.py
@@ -1,60 +1,38 @@
 from mysql import connector
+import logging
 import csv
 from time import sleep
 
-
 class search():
     mydb = connector.connect(
-        host='192.168.0.172',
+        host='192.168.0.195',
         user='root',
         passwd='root',
         database='mirror',
         charset='utf8'
     )
-    sql = "INSERT INTO mr_host (ck,name) VALUES ('11111111111111111111111111', 'wwww')"
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
-    mydb.commit()
 
+    i = 150
+    # sql = "INSERT INTO mr_host (ck, name, mac, is_auth) VALUES (' +  str(i), str(i), str(i), 1 + ')"
 
-
-
-
-
-
-
-
-
-    def DeletTable(self,table, number):
-        sql = "DELETE FROM" + " " + table + " " + "WHERE id REGEXP"
-        num = "^" + "[0-" + number + "]"
-        mycursor = self.mydb.cursor()
-        mycursor.execute(sql + ' ''\'' + num + '\'')
-        sleep(50)
-        self.mydb.commit()
-
-    # 清空表
-    def DeletAll(self,table):
-        sql = "truncate" + " " + table
-        mycursor = self.mydb.cursor()
+    for i in range(160, 200):
+        sqlpre = "INSERT INTO mr_host (ck, name, mac, is_auth) VALUES ("
+        sqlend = ")"
+        sqlsplit = ","
+        sqlck = "\'" + str(i) + "\'"
+        sqlname = "\'" + str(i) + "\'"
+        sqlmac = "\'" + str(i) + "\'"
+        sqlisauth = str(1)
+        sql = sqlpre + sqlck + sqlsplit + sqlname + sqlsplit + sqlmac + sqlsplit + sqlisauth + sqlend
+        print(sql)
+        logging.info(sql)
+        # sql = "INSERT INTO mr_host (ck, name, mac, is_auth) VALUES ('zx11','zx11','zx11', 1)"
+        mycursor = mydb.cursor()
         mycursor.execute(sql)
+        mydb.commit()
 
-    # 读取csv文件
-    def read(self):
-        with open("E:\\table.csv", "r", encoding="utf-8") as csvfile:
-            read = csv.reader(csvfile)
-            for i in read:
-                return i[0]
+    # mycursor.close()
+    # mydb.close()
 
-    def cretestring(self):
-        x = 0
-        for i in range(2000):
-            x += 1
-            s = str(x)
 
-    #填充数据库
-    def create(self):
-        sql = "INSERT INTO mr_host (ck, name) VALUES ('111', 'wwww')"
-        mycursor = self.mydb.cursor()
-        mycursor.execute(sql)
 
